33383b977193a97ae5f36b8125905659983583ba
fzzlfzzl <fzzlfzzl@3b4a4e4e-1a45-40f8-a198-9b1cfc5ad0d6>
2015-02-20 16:17:26 +0000

    spring hibernate配置完成，并配置dev环境

--- src/main/java/com/controller/SiteController.java
+++ src/main/java/com/controller/SiteController.java
@@ -3,10 +3,15 @@ package com.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.dao.bean.Address;
+import com.dao.bean.User;
+import com.dao.db.HibernateUtil;
 
 @Controller
 @RequestMapping("/site")
@@ -16,6 +21,19 @@ public class SiteController {
 	public ModelAndView index(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		ModelAndView ret = new ModelAndView("index");
+		try {
+			Session session = HibernateUtil.openSession();
+			session.beginTransaction();
+			User user = new User();
+			Address addr = new Address();
+			addr.setAddress("" + System.currentTimeMillis());
+			user.getAddressList().add(addr);
+			session.getTransaction().commit();
+			session.close();
+			ret.addObject("result", "succ");
+		} catch (Exception e) {
+			ret.addObject("result", "err");
+		}
 		return ret;
 	}
 }
--- src/main/java/com/dao/db/HibernateUtil.java
+++ src/main/java/com/dao/db/HibernateUtil.java
@@ -1,5 +1,6 @@
 package com.dao.db;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -7,12 +8,22 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
+	private static Logger logger = Logger.getLogger(HibernateUtil.class);
+
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
 		try {
-			return new Configuration().configure().buildSessionFactory(
-					new StandardServiceRegistryBuilder().build());
+			if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+				logger.info("Development Environment");
+				return new Configuration().configure("hibernate.dev.cfg.xml")
+						.buildSessionFactory(
+								new StandardServiceRegistryBuilder().build());
+			} else {
+				logger.info("Online Environment");
+				return new Configuration().configure().buildSessionFactory(
+						new StandardServiceRegistryBuilder().build());
+			}
 		} catch (Throwable ex) {
 			System.err.println("Initial SessionFactory creation failed." + ex);
 			throw new ExceptionInInitializerError(ex);
